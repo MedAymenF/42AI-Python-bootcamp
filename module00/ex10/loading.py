95482647ffce079067d38635c7b69ebaafb24faa
Mohamed Aymane Farhi <aymenfarhi.25@gmail.com>
2021-01-18 22:42:46 +0100

    ex10

--- module00/ex10/loading.py
+++ module00/ex10/loading.py
@@ -6,7 +6,9 @@ def ft_progress(lst):
     length = len(lst)
     for i in lst:
         os.system('clear')
-        print("[{:3}%]".format((i + 1) * 100 // length), end=' ')
+        progress = (i + 1) * 100 // length
+        print("[{:3}%]".format(progress), end=' ')
+        print("[{:25}]".format("=" * (24 * progress // 100) + ">"), end=' ')
         print(f"{i}/{length}")
         yield i
 
@@ -15,7 +17,6 @@ listy = range(1000)
 ret = 0
 for elem in ft_progress(listy):
     ret += (elem + 3) % 5
-    # print(f"elem = {elem}, ret = {ret}")
     sleep(0.01)
 print()
 print(ret)
