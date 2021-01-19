8dd902eb25cbfaa07101decdccd77ce179a9d673
Mohamed Aymane Farhi <aymenfarhi.25@gmail.com>
2021-01-20 00:28:55 +0100

    ex02

--- module01/ex02/test.py
+++ module01/ex02/test.py
@@ -3,5 +3,9 @@ from vector import Vector
 
 v1 = Vector([0.0, 1.0, 2.0, 3.0])
 v2 = Vector(3)
+v3 = Vector((10, 16))
 print(v1.__dict__)
 print(v2.__dict__)
+print(v3.__dict__)
+print(str(v3))
+print(v3)
--- module01/ex02/vector.py
+++ module01/ex02/vector.py
@@ -1,4 +1,28 @@
 class Vector:
-    def __init__(self, values=[], size=0):
-        self.values = values
-        self.size = size
+    def __init__(self, arg):
+        if (type(arg) is int):
+            self.size = arg
+            self.values = [0.0 + i for i in range(arg)]
+        elif (type(arg) is list):
+            for elem in arg:
+                if (type(elem) is not float):
+                    exit()
+            self.size = len(arg)
+            self.values = arg
+        elif (type(arg) is tuple):
+            if (len(arg) != 2 or type(arg[0]) is not int or type(arg[1]) is not int):
+                exit()
+            self.size = arg[1] - arg[0]
+            if (self.size < 0):
+                exit()
+            self.values = [0.0 + i for i in range(arg[0], arg[1])]
+        else:
+            exit()
+
+    def __repr__(self):
+        txt = str(self.values)
+        return txt
+
+    def __str__(self):
+        txt = str(self.values)
+        return f"Vector ({txt})"
