e74282efe660b59e5ec46004a47effde16be7a3a
Mohamed Aymane Farhi <aymenfarhi.25@gmail.com>
2021-01-22 00:03:00 +0100

    ex05

--- module01/ex05/eval.py
+++ module01/ex05/eval.py
@@ -4,17 +4,22 @@ class Evaluator:
         if (len(coefs) != len(words)):
             return -1
         return sum([coeff * len(word) for (coeff, word)
-                    in list(zip(coefs, words))])
+                    in zip(coefs, words)])
 
     @staticmethod
     def enumerate_evaluate(coefs, words):
         if (len(coefs) != len(words)):
             return -1
+        return sum([coef * len(words[i]) for (i, coef) in enumerate(coefs)])
 
 
 words = ["Le", "Lorem", "Ipsum", "est", "simple"]
 coefs = [1.0, 2.0, 1.0, 4.0, 0.5]
 print(Evaluator.zip_evaluate(coefs, words))
+
+words = ["Le", "Lorem", "Ipsum", "est", "simple"]
+coefs = [1.0, 2.0, 1.0, 4.0, 0.5]
+print(Evaluator.enumerate_evaluate(coefs, words))
 
 words = ["Le", "Lorem", "Ipsum", "n'", "est", "pas", "simple"]
 coefs = [0.0, -1.0, 1.0, -12.0, 0.0, 42.42]
