ff5517b618597b5f9de2bfbb7979d459c05ee5c3
Mohamed Aymane Farhi <mfarhi@e1r5p3.1337.ma>
2021-01-22 15:00:07 +0100

    ex06

--- module01/ex06/the_bank.py
+++ module01/ex06/the_bank.py
@@ -19,20 +19,93 @@ class Bank(object):
         self.account = []
 
     def add(self, account):
-        self.account.append(account)
+        if (type(account) is not Account):
+            print('Acoount must be an `Account` object')
+        else:
+            self.account.append(account)
+
+    @staticmethod
+    def is_corrupted(account):
+        if (len(dir(account)) % 2 == 0):
+            return True
+        if ('name' not in dir(account)
+                or 'id' not in dir(account) or 'value' not in dir(account)):
+            return True
 
     def transfer(self, origin, dest, amount):
         """
-        @origin: int(id) or str(name) of the first account
-        @dest: int(id) or str(name) of the destination account
-        @amount: float(amount) amount to transfer
-        @return True if success, False if an error occured
+            @origin: int(id) or str(name) of the first account
+            @dest: int(id) or str(name) of the destination account
+            @amount: float(amount) amount to transfer
+            @return True if success, False if an error occured
         """
+
+        try:
+            assert type(origin) is int or type(origin) is str
+            assert type(dest) is int or type(dest) is str
+            assert type(amount) is float
+        except AssertionError as ae:
+            print("Type error")
+            return False
+        try:
+            assert amount >= 0
+        except AssertionError as ae:
+            print("Transfer error")
+            return False
+
+        origin_acc = None
+        if (type(origin) is int):
+            for account in self.account:
+                if (account.id == origin):
+                    origin_acc = account
+                    break
+        else:
+            for account in self.account:
+                if (account.name == origin):
+                    origin_acc = account
+                    break
+
+        dest_acc = None
+        if (type(dest) is int):
+            for account in self.account:
+                if (account.id == dest):
+                    dest_acc = account
+                    break
+        else:
+            for account in self.account:
+                if (account.name == dest):
+                    dest_acc = account
+                    break
+
+        if (origin_acc is None or dest_acc is None):
+            print("Account not found")
+            return False
+
+        if (self.is_corrupted(origin_acc) or self.is_corrupted(dest_acc)):
+            print("Account corrupted")
+            return False
+
+        if (origin_acc.value < amount):
+            print("Funds insufficient")
+            return False
+
+        dest_acc.value += amount
+        return True
 
     def fix_account(self, account):
         """
-        fix the corrupted account
-        @account: int(id) or str(name) of the account
-        @return
-        True if success, False if an error occured
+            fix the corrupted account
+            @account: int(id) or str(name) of the account
+            @return
+            True if success, False if an error occured
         """
+
+
+my_acc = Account("Aymane", addr="Kh", zip="30090")
+my_acc.value = 1000.0
+acc2 = Account("Jon", value=0)
+bank = Bank()
+bank.add(my_acc)
+bank.add(acc2)
+if (bank.transfer(my_acc.id, acc2.id, 100.0)):
+    print("Success")
