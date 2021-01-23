8ea4e0e3a3d8bf5ec1616fc4dab201c51b3ca96c
Mohamed Aymane Farhi <mfarhi@e1r5p3.1337.ma>
2021-01-23 12:58:43 +0100

    getdata()

--- module02/ex03/csvreader.py
+++ module02/ex03/csvreader.py
@@ -4,16 +4,25 @@ class CsvReader():
         self.sep = sep
         self.skip_top = skip_top
         self.skip_bottom = skip_bottom
+
         try:
             self.file = open(filename, 'r')
         except Exception as e:
             print("Problem opening file")
             raise e
             return None
+
         if header:
             self.header = self.file.readline()
         else:
             self.header = None
+
+        self.data = self.file.readlines()
+        self.data = [line.strip() for line in self.data]
+        self.data = [line.split(sep=self.sep) for line in self.data]
+        for line in self.data:
+            for field in line:
+                field.strip()
 
     def __enter__(self):
         return self
@@ -22,11 +31,10 @@ class CsvReader():
         self.file.close()
 
     def getdata(self):
-        return self.file.readline().split(sep=self.sep)
+        return self.data
 
     def getheader(self):
         return self.header
-
 
 
 if __name__ == "__main__":
