27f1e317f8d06568583b9bdd0d5c00a0f79d7cea
Mohamed Aymane Farhi <mfarhi@e1r5p3.1337.ma>
2021-01-25 12:58:49 +0100

    Saving

--- module03/ex02/ScrapBooker.py
+++ module03/ex02/ScrapBooker.py
@@ -1,6 +1,13 @@
+import numpy as np
+from ImageProcessor import ImageProcessor
+
+
 class ScrapBooker:
-    def crop(self, array, dimensions, position):
-        pass
+    def crop(self, array, dimensions, position=(0, 0)):
+        if (dimensions[0] > array.shape[0] or dimensions[1] > array.shape[1]):
+            print("Incorrect dimensions")
+            return None
+        return array
 
     def thin(self, array, n, axis):
         pass
@@ -10,3 +17,11 @@ class ScrapBooker:
 
     def mosaic(self, array, dimensions):
         pass
+
+
+sb = ScrapBooker()
+
+imp = ImageProcessor()
+arr = imp.load("42AI.png")
+arr = sb.crop(arr, (100, 100), (0, 0))
+imp.display(arr)
