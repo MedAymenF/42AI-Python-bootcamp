87d42ca80ffa2397d1e558f2db043baa5e6bf852
Mohamed Aymane Farhi <mfarhi@e1r5p3.1337.ma>
2021-01-31 17:07:11 +0100

    ex06

--- module04/ex06/MyPlotLib.py
+++ module04/ex06/MyPlotLib.py
@@ -1,5 +1,25 @@
-import numpy as np
+from FileLoader import FileLoader
+import matplotlib.pyplot as plt
 
 
 class MyPlotLib:
-    pass
+    def histogram(self, data, features):
+        for i, feature in enumerate(features):
+            ax = plt.subplot(1, 2, i + 1)
+            num_bins = 10
+            ax.hist(data[feature], num_bins, facecolor='blue')
+            ax.set_title(feature)
+        plt.tight_layout()
+        plt.show()
+
+    def density(self, data, features):
+        # Seaborn just won't work
+        pass
+
+
+if __name__ == "__main__":
+    loader = FileLoader()
+    data = loader.load('athlete_events.csv')
+    mpl = MyPlotLib()
+    mpl.histogram(data, ['Height', 'Weight'])
+    # mpl.density(data, ['Height', 'Weight'])
